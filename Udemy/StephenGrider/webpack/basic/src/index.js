7df2b3e6173af6d549136918768d4499c7b012b3
Sean Hong <iamseanhong@gmail.com>
2017-12-10 15:35:17 -0800

    Add output path and filename to webpack.config.js

--- Udemy/StephenGrider/webpack/basic/src/index.js
+++ Udemy/StephenGrider/webpack/basic/src/index.js
@@ -1,11 +1,4 @@
 const sum = require('./sum');
 
 const total = sum(1, 2);
-const some = require('./sum')
-const a = require('.')
-
-const config = {
-  // entry: './index.js'
-}
-
-module.exports = config;
+console.log('total: ', total);

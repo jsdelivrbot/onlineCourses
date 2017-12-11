90f8bb0c07a1c1802fabac20aaaa53f47b1f35e1
Sean Hong <iamseanhong@gmail.com>
2017-12-10 18:28:55 -0800

    Run image_viewer.js by importing

--- Udemy/StephenGrider/webpack/basic/src/index.js
+++ Udemy/StephenGrider/webpack/basic/src/index.js
@@ -2,10 +2,12 @@
 // babel is one of the most popular loader
 // it has three parts babel-loader babel-core babel-preset-env
 
+// style loader and css loader
 
 // const sum = require('./sum');
 
 import sum from './sum';
+import './image_viewer';
 
 const total = sum(1, 2);
 console.log('total: ', total);

97869615b791a88fe87f177ec5a2597050b7de1b
Sean Hong <iamseanhong@gmail.com>
2017-12-10 18:00:25 -0800

    Add babel into webpack

--- Udemy/StephenGrider/webpack/basic/src/index.js
+++ Udemy/StephenGrider/webpack/basic/src/index.js
@@ -1,3 +1,8 @@
+// loader: enhance the ability of webpack
+// babel is one of the most popular loader
+// it has three parts babel-loader babel-core babel-preset-env
+
+
 const sum = require('./sum');
 
 const total = sum(1, 2);

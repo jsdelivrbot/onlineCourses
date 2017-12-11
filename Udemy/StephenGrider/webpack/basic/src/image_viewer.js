57954ec1396f38ea0e1daf84262a122c27422147
Sean Hong <iamseanhong@gmail.com>
2017-12-10 19:26:35 -0800

    Able to render small img

--- Udemy/StephenGrider/webpack/basic/src/image_viewer.js
+++ Udemy/StephenGrider/webpack/basic/src/image_viewer.js
@@ -1,5 +1,17 @@
+
+import big from '../assets/big.jpg';
+import small from '../assets/small.jpg';
+
 import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = 'https://lorempixel.com/400/400';
-document.body.appendChild(image);
+// const image = document.createElement('img');
+// image.src = 'https://lorempixel.com/400/400';
+// document.body.appendChild(image);
+
+const smallImg = document.createElement('img');
+smallImg.src = small;
+document.body.appendChild(smallImg);
+
+const bigImg = document.createElement('img');
+bigImg.src = big;
+document.body.appendChild(bigImg);

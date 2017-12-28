a6e4b11627a2fa2ce7995ab72c5757929613a89c
Sean Hong <iamseanhong@gmail.com>
2017-12-28 15:31:48 -0700

    Connect to graphiql by app.use

--- Udemy/StephenGrider/GraphQL/basicUsers/server.js
+++ Udemy/StephenGrider/GraphQL/basicUsers/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const expressGraphQL= require('express-graphql');
 
 const app = express();
 
 const PORT = 3000;
+
+app.use('/graphiql', expressGraphQL({
+  graphiql: true
+}));
 
 app.listen(PORT, (req, res) => {
   console.log(`App is listening to port: ${PORT}`);

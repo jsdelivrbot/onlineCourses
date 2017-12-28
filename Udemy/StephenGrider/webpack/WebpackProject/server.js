e4c6e9bc8c29cfeb75d922945da7a5910355e99b
Sean Hong <iamseanhong@gmail.com>
2017-12-28 14:09:06 -0700

    Finish webpack course

--- Udemy/StephenGrider/webpack/WebpackProject/server.js
+++ Udemy/StephenGrider/webpack/WebpackProject/server.js
@@ -4,17 +4,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-// if (process.env.NODE_ENV !== 'production') {
-//   const WebpackDevMiddleware= require('webpack-dev-middleware');
-//   const webpack = require('webpack');
-//   const webpackConfig = require('./webpack.config');
-//   app.use(WebpackDevMiddleware(webpack(webpackConfig)));
-// } else {
+if (process.env.NODE_ENV !== 'production') {
+  const WebpackDevMiddleware= require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config');
+  app.use(WebpackDevMiddleware(webpack(webpackConfig)));
+} else {
   app.use(express.static('dist'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   })
-// }
+}
 
 
 app.listen(PORT, () => {

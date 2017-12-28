cc8bca80a379c6fd0ee71e43b1ad561829d05200
Sean Hong <iamseanhong@gmail.com>
2017-12-27 22:51:44 -0700

    Deploying using heroku

---
+++ Udemy/StephenGrider/webpack/WebpackProject/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// if (process.env.NODE_ENV !== 'production') {
+//   const WebpackDevMiddleware= require('webpack-dev-middleware');
+//   const webpack = require('webpack');
+//   const webpackConfig = require('./webpack.config');
+//   app.use(WebpackDevMiddleware(webpack(webpackConfig)));
+// } else {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  })
+// }
+
+
+app.listen(PORT, () => {
+  console.log(`listeing to the port ${PORT}`);
+})

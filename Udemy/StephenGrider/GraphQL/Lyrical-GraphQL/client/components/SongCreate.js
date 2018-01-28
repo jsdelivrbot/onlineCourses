4cd06196501343de3c84d63e32f7fe91e2650b85
Sean Hong <iamseanhong@gmail.com>
2018-01-28 12:22:05 -0800

    Able to add song title with graphql mutate

--- Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/components/SongCreate.js
+++ Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/components/SongCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import gpl from 'graphql-tag';
+import {graphql} from 'react-apollo';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -6,14 +8,25 @@ class SongCreate extends Component {
     this.state = {
       title: '',
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.props);
+    this.props.mutate({
+      variables: {
+        title: this.state.title
+      }
+    });
   }
 
   render() {
     return (
       <div>
         <h3>Song Create</h3>
-        <form action="">
-          <label>song title</label>
+        <form onSubmit={this.handleSubmit} action="">
+          <label>Song Title:</label>
           <input type="text" onChange={e => this.setState({ title: e.target.value })} value={this.state.title} />
         </form>
       </div>
@@ -21,4 +34,21 @@ class SongCreate extends Component {
   }
 }
 
-export default SongCreate;
+const mutation = gpl`
+mutation AddSong ($title: String){
+  addSong(title: $title) {
+    id,
+    title
+  }
+}
+`;
+
+// const mutation = gpl`
+//   addSong() {
+//     id,
+//       title
+//   }
+// `;
+
+export default graphql(mutation)(SongCreate);
+// export default SongCreate;

0ca601555d12c51e5e907a8ab64d08f68d60a8cd
Sean Hong <iamseanhong@gmail.com>
2018-01-27 22:12:58 -0800

    Remove .git in nested folder

---
+++ Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+import App from './components/App'
+import SongList from './components/SongList';
+
+const client = new ApolloClient({});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector('#root'));

759a4661df34c4409e886e3e83757ef1d50b83fe
Sean Hong <iamseanhong@gmail.com>
2018-01-28 16:11:55 -0800

    Refetch using this.props.data.refetch()

--- Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/components/SongList.js
+++ Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/components/SongList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import gpl from 'graphql-tag';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 import { Link } from 'react-router';
 
 // graphql
@@ -8,11 +8,25 @@ import fetchSongs from '../queries/fetchSongs';
 
 class SongList extends Component {
   renderSongs() {
-    return (
-      this.props.data.songs.map((song) => {
-        return <li className="collection-item" key={song.id}>{song.title}</li>;
-      })
-    );
+    return this.props.data.songs.map(song => {
+      return (
+        <li className="collection-item" key={song.id}>
+          {song.title}
+          <span
+            onClick={() => {
+              console.log('this.props: ', this.props);
+              this.props.mutate({variables: { id: [song.id] }})
+                .then(() => {
+                  this.props.data.refetch();
+                });
+            }}
+            style={{ color: 'red', cursor: 'pointer', float: 'right' }}
+          >
+            delete
+          </span>
+        </li>
+      );
+    });
   }
 
   render() {
@@ -20,17 +34,33 @@ class SongList extends Component {
     console.log('this.props.data: ', this.props.data);
     return (
       <div>
-      <ul className="collection">
-        {this.renderSongs()}
-      </ul>
-      <Link to='/songs/new' className="btn-floating btn-large red right">
-        <i className="material-icons"> add </i>
-      </Link>
+        <ul className="collection">{this.renderSongs()}</ul>
+        <Link to="/songs/new" className="btn-floating btn-large red right">
+          <i className="material-icons"> add </i>
+        </Link>
       </div>
     );
   }
 }
 
+const mutation = gpl`
+mutation DeleteSong ($id:ID) {
+  deleteSong(id: $id) {
+    id
+  }
+}
+`;
 
+// export default compose(graphql(fetchSongs)(SongList), graphql(mutation)(SongList));
+export default compose(graphql(fetchSongs), graphql(mutation))(SongList);
 
-export default graphql(fetchSongs)(SongList);
+// export default graphql(mutation)(
+//   graphql(fetchSongs)(SongList)
+// );
+
+// export default graphql(fetchSongs)(SongList);
+
+// export default compose(
+//   graphql(mutation),
+//   graphql(fetchSongs),
+// )(SongList);

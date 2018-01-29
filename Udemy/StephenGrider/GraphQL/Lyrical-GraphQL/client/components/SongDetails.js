f96457847d65d2c9875041f64c5bd90040160044
Sean Hong <iamseanhong@gmail.com>
2018-01-28 17:11:34 -0800

    Fetching with id by using options porps variable ...

--- Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/components/SongDetails.js
+++ Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/components/SongDetails.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+// queries
+import fetchSongById from '../queries/fetchSongById';
 
 class SongDetails extends Component {
   render() {
+    console.log('this.props: ', this.props);
     return (
       <div>
         <h3> Song Details </h3>
       </div>
-    )
+    );
   }
 }
 
-export default SongDetails;
+export default graphql(fetchSongById, {
+  options: props => {
+    return {
+      variables: {
+        id: props.params.id,
+      },
+    };
+  },
+})(SongDetails);
+// export default SongDetails;
--- Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/queries/fetchSongById.js
+++ Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/queries/fetchSongById.js
@@ -1,10 +1,10 @@
 import gpl from 'graphql-tag';
 
-export default gpl = `
+export default gpl`
 query fetchSongById($id: ID!) {
-  song(id: $id) {
-    id,
-    title
-  } 
+ song(id: $id) {
+  id,
+  title
+} 
 }
 `;

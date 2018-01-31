870b026a6a21cc58b3ec23278b4720f77b2abbdd
Sean Hong <iamseanhong@gmail.com>
2018-01-30 21:22:45 -0800

    Add thumb_up icon to lyric list

--- Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/components/LyricList.js
+++ Udemy/StephenGrider/GraphQL/Lyrical-GraphQL/client/components/LyricList.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
 export default class LyricList extends Component {
+  onLink(id) {
+    console.log('id: ', id);
+  }
+
   renderLyrics() {
-    return this.props.lyrics.map(({id, content}) => {
+    return this.props.lyrics.map(({ id, content }) => {
       return (
         <li key={id} className="collection-item">
           {content}
+          <i
+            onClick={() => {
+              this.onLink(id);
+            }}
+            className="material-icons"
+          >
+            thumb_up
+          </i>
         </li>
       );
     });
